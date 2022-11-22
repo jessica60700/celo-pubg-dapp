2924da44af2384e6c5dada46c50d60652c002c7a
jessica60700 <116009448+jessica60700@users.noreply.github.com>
2022-11-22 11:30:18 +0100

    Update minter.js

--- src/utils/minter.js
+++ src/utils/minter.js
@@ -1,136 +1,173 @@
-import {create as ipfsHttpClient} from "ipfs-http-client";
+import { create as ipfsHttpClient } from "ipfs-http-client";
 import axios from "axios";
-import {Web3Storage} from 'web3.storage/dist/bundle.esm.min.js'
+import BigNumber from "bignumber.js";
+import { ERC20_DECIMALS } from "./constants";
 
-// initialize IPFS
-const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+const projectId = '2Hg4SbgdyjXFLmFIANyGlVQhYCV';
+const projectSecret = '702ab90817e45dcd762fe53ee9c22475';
+const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString(
+  'base64',
+)}`;
+
+const client = ipfsHttpClient({
+	host: "ipfs.infura.io",
+	port: 5001,
+	protocol: "https",
+	apiPath: "/api/v0",
+	headers: {
+		authorization: auth,
+	},
+});
 
 // mint an NFT
 export const createNft = async (
-    minterContract,
-    performActions,
-    {name, description, ipfsImage, ownerAddress, attributes}
+	minterContract,
+	performActions,
+	{ name, description, image, price }
 ) => {
-    await performActions(async (kit) => {
-        if (!name || !description || !ipfsImage) return;
-        const {defaultAccount} = kit;
+	await performActions(async (kit) => {
+		if (!name || !description || !image) return;
+		const { defaultAccount } = kit;
 
-        // convert NFT metadata to JSON format
-        const data = JSON.stringify({
-            name,
-            description,
-            image: ipfsImage,
-            owner: defaultAccount,
-            attributes,
-        });
+		// convert NFT metadata to JSON format
+		const data = JSON.stringify({
+			name,
+			description,
+			image,
+		});
+		try {
+			price = new BigNumber(price).shiftedBy(ERC20_DECIMALS).toString();
 
-        try {
+			// save NFT metadata to IPFS
+			const added = await client.add(data);
 
-            // save NFT metadata to IPFS
-            const added = await client.add(data);
+			// IPFS url for uploaded metadata
+			const url = `https://pugb.infura-ipfs.io/ipfs/${added.path}`;
 
-            // IPFS url for uploaded metadata
-            const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+			// mint the NFT and save the IPFS url to the blockchain
+			let transaction = await minterContract.methods
+				.mintCard(url, price)
+				.send({ from: defaultAccount });
 
-            // mint the NFT and save the IPFS url to the blockchain
-            let transaction = await minterContract.methods
-                .safeMint(ownerAddress, url)
-                .send({from: defaultAccount});
-
-            return transaction;
-        } catch (error) {
-            console.log("Error uploading file: ", error);
-        }
-    });
+			return transaction;
+		} catch (error) {
+			console.log("Error uploading file: ", error);
+		}
+	});
 };
-
 
 // function to upload a file to IPFS
 export const uploadToIpfs = async (e) => {
-    const file = e.target.files[0];
-    if (!file) return;
-    try {
-        const added = await client.add(file, {
-            progress: (prog) => console.log(`received: ${prog}`),
-        });
-        return `https://ipfs.infura.io/ipfs/${added.path}`;
-    } catch (error) {
-        console.log("Error uploading file: ", error);
-    }
+	const file = e.target.files[0];
+	if (!file) return;
+	try {
+		const added = await client.add(file, {
+			progress: (prog) => console.log(`received: ${prog}`),
+		});
+		return `https://pugb.infura-ipfs.io/ipfs/${added.path}`;
+	} catch (error) {
+		console.log("Error uploading file: ", error);
+	}
 };
-
-// function to upload a file to IPFS via web3.storage
-export const uploadFileToWebStorage = async (e) => {
-    // Construct with token and endpoint
-    const client = new Web3Storage({token: process.env.REACT_APP_STORAGE_API_KEY})
-
-    const file = e.target.files;
-    if (!file) return;
-    // Pack files into a CAR and send to web3.storage
-    const rootCid = await client.put(file) // Promise<CIDString>
-
-    // Fetch and verify files from web3.storage
-    const res = await client.get(rootCid) // Promise<Web3Response | null>
-    const files = await res.files() // Promise<Web3File[]>
-
-    return `https://ipfs.infura.io/ipfs/${files[0].cid}`;
-}
-
 
 // fetch all NFTs on the smart contract
 export const getNfts = async (minterContract) => {
-    try {
-        const nfts = [];
-        const nftsLength = await minterContract.methods.totalSupply().call();
-        for (let i = 0; i < Number(nftsLength); i++) {
-            const nft = new Promise(async (resolve) => {
-                const res = await minterContract.methods.tokenURI(i).call();
-                const meta = await fetchNftMeta(res);
-                const owner = await fetchNftOwner(minterContract, i);
-                resolve({
-                    index: i,
-                    owner,
-                    name: meta.data.name,
-                    image: meta.data.image,
-                    description: meta.data.description,
-                    attributes: meta.data.attributes,
-                });
-            });
-            nfts.push(nft);
-        }
-        return Promise.all(nfts);
-    } catch (e) {
-        console.log({e});
-    }
+	try {
+		const _nfts = await minterContract.methods.getCards().call();
+		const nfts = await Promise.all(
+			_nfts.map(async (card) => {
+				// prevents retrieval of data for deleted cards
+				if (
+					card.owner === "0x0000000000000000000000000000000000000000"
+				) {
+					return;
+				}
+				const tokenUri = await minterContract.methods
+					.tokenURI(card.index)
+					.call();
+				const meta = await fetchNftMeta(tokenUri);
+				return {
+					tokenId: Number(card.index),
+					price: new BigNumber(card.price),
+					owner: card.owner,
+					onSale: card.onSale,
+					name: meta.data.name,
+					description: meta.data.description,
+					image: meta.data.image,
+				};
+			})
+		);
+		return nfts;
+	} catch (e) {
+		console.log({ e });
+	}
 };
 
 // get the metedata for an NFT from IPFS
 export const fetchNftMeta = async (ipfsUrl) => {
-    try {
-        if (!ipfsUrl) return null;
-        const meta = await axios.get(ipfsUrl);
-        return meta;
-    } catch (e) {
-        console.log({e});
-    }
+	try {
+		if (!ipfsUrl) return null;
+		const meta = await axios.get(ipfsUrl);
+		return meta;
+	} catch (e) {
+		console.log({ e });
+	}
 };
 
-
-// get the owner address of an NFT
-export const fetchNftOwner = async (minterContract, index) => {
-    try {
-        return await minterContract.methods.ownerOf(index).call();
-    } catch (e) {
-        console.log({e});
-    }
+export const buyCard = async (minterContract, performActions, index, price) => {
+	try {
+		await performActions(async (kit) => {
+			const { defaultAccount } = kit;
+			await minterContract.methods
+				.buyCard(index)
+				.send({ from: defaultAccount, value: price });
+		});
+	} catch (error) {
+		console.error({ error });
+	}
 };
 
-// get the address that deployed the NFT contract
-export const fetchNftContractOwner = async (minterContract) => {
-    try {
-        let owner = await minterContract.methods.owner().call();
-        return owner;
-    } catch (e) {
-        console.log({e});
-    }
+export const sellCard = async (
+	minterContract,
+	performActions,
+	index,
+	price
+) => {
+	try {
+		price = new BigNumber(price).shiftedBy(ERC20_DECIMALS).toString();
+		await performActions(async (kit) => {
+			const { defaultAccount } = kit;
+			await minterContract.methods
+				.sellCard(index, price)
+				.send({ from: defaultAccount });
+		});
+	} catch (error) {
+		console.error({ error });
+	}
+};
+
+export const unlistCard = async (minterContract, performActions, index) => {
+	try {
+		await performActions(async (kit) => {
+			const { defaultAccount } = kit;
+			await minterContract.methods
+				.unlistCard(index)
+				.send({ from: defaultAccount });
+		});
+	} catch (error) {
+		console.error({ error });
+	}
+};
+
+export const deleteCard = async (minterContract, performActions, index) => {
+	try {
+		await performActions(async (kit) => {
+			const { defaultAccount } = kit;
+			await minterContract.methods
+				.deleteCard(index)
+				.send({ from: defaultAccount });
+		});
+	} catch (error) {
+		console.error({ error });
+	}
 };
